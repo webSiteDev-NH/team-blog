8e22ecf14442cc61046527312738bb1f7135d0fd
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2020-01-29 10:15:20 +0900

    add_devise

---
+++ app/models/team.rb
@@ -0,0 +1,10 @@
+class Team < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :representative, presence: true
+  validates :team_name, presence: true
+
+end
