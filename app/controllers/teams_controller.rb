59fe367a9b845138e4063471ed65da6eced3e376
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2020-01-29 20:43:38 +0900

    add_logout-user_function

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,9 @@
 class TeamsController < ApplicationController
 
+  def index
+    @teams = Team.all
+  end
+
   def show
     @team = Team.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :teams
   root "posts#index"
   resources :posts
-  resources :teams, only: :show
+  resources :teams, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
