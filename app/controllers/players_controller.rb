00cd1abe67228807bd0dd3ab403356db36d64beb
hiroki-n-8 <nonomurahiroki0807@gmail.com>
2020-02-04 20:56:17 +0900

    add_carrierwave_mini-magick

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,6 @@
 class PlayersController < ApplicationController
 
   before_action :player_find, only: [:show, :edit, :update, :destroy]
-  before_action :current_team, only: [:new, :create, :edit, :destroy]
 
   def index
     @players = Player.all.includes(:team)
@@ -26,6 +25,7 @@ class PlayersController < ApplicationController
   end
 
   def edit
+    @team = @player.team
   end
 
   def update
@@ -37,6 +37,7 @@ class PlayersController < ApplicationController
   end
 
   def destroy
+    # @team = current_team.id
     if @player.destroy
       redirect_to affiliation_team_path(@team), notice:"削除できました"
     else
@@ -50,12 +51,8 @@ class PlayersController < ApplicationController
     @player = Player.find(params[:id])
   end
 
-  #新規作成(new,create)、削除(destroy)後に所属選手一覧(affiliation)へ遷移する時にチームidを渡す
-  def current_team
-    @team = current_team.id
+  def profile_params
+    params.require(:player).permit(:name, :number, :position, :birthday, :height, :weight, :goals, :play_style, :image).merge(team_id: current_team.id)
   end
 
-  def profile_params
-    params.require(:player).permit(:name, :number, :position, :birthday, :height, :wight, :goals, :play_style, :image).merge(team_id: current_team.id)
-  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -4,4 +4,7 @@ class Player < ApplicationRecord
   validates :position, presence: true
 
   belongs_to :team
+
+  mount_uploader :image, ImageUploader
+  
 end
